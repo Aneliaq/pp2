1cf68d69801d2a197ab6ee5716f8d79ce98a23bb
Anelia Bakytzhankyzy <aneliabakytzhankyzy@MacBook-Air-Anelia.local>
2026-01-27 11:44:12 +0500

    Add Python W3Schools exercises

---
+++ w3schools/python/06_variables.py
@@ -0,0 +1,89 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, y, z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+x = 'John'
+print(x)
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+    print("Python is " + x)
+myfunc()
+
+x = "awesome"
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+def myfunc():
+    global x
+    x = "fantastic"
+myfunc()
+print("Python is " + x)
+
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+myfunc()
+print("Python is " + x)
