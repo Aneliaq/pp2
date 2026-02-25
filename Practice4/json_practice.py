287175848b492c644cbc38d59f89ed5dc40878b4
Anelia Bakytzhankyzy <aneliabakytzhankyzy@MacBook-Air-Anelia.local>
2026-02-25 12:12:56 +0500

    Update JSON parsing exercise

--- Practice4/json_practice.py
+++ Practice4/json_practice.py
@@ -1,18 +1,15 @@
 import json
 
-# 1. Открываем JSON файл
 with open("sample-data.json") as f:
     data = json.load(f)
 
-# 2. Заголовки таблицы
 print("Interface Status")
 print("=" * 80)
 print(f"{'DN':50} {'Description':20} {'Speed':6} {'MTU':6}")
 print("-" * 50 + " " + "-" * 20 + " " + "-" * 6 + " " + "-" * 6)
 
-# 3. Проходим по данным и выводим строки
 for item in data["imdata"]:
-    iface = item["l1PhysIf"]["attributes"]  # <-- исправлено под твою структуру
+    iface = item["l1PhysIf"]["attributes"]
     dn = iface["dn"]
     descr = iface.get("descr", "")
     speed = iface.get("speed", "")
